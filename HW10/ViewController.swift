aa87fb51c73d4dba1bdbc473f634ed43d7da084f
Nikita <hoyshik@yandex.by>
2024-08-05 22:21:39 +0300

    Добавил дз 10

---
+++ HW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW10
+//
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
